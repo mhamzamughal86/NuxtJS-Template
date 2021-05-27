240ca9bcd25e7f2fa1cdf3bb2f97a838a9a44664
Muhammad Saeed <Muhammad.Saeed@CLICK.aero>
2021-05-27 11:48:56 +0400

    Project created

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-center">Welcome to NuxtJS Template</h1>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
