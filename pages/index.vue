87e5aac854c33d05dc1d615d48c7f66903e399fe
Muhammad Saeed <Muhammad.Saeed@CLICK.aero>
2021-06-06 09:57:40 +0400

    Add lodash as plugin for globally use

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,34 @@
 <template>
-  <h1 class="text-center">Welcome to NuxtJS Template</h1>
+  <v-container>
+    <h1>Welcome to nuxtjs template</h1>
+    <h1>{{getUser}}</h1>
+  </v-container>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   components: {
     Logo,
     VuetifyLogo
   },
+  data:()=>({
+    users :[
+      {name: 'Hamza', blocked: true},
+      {name: 'Basit', blocked: false},
+      {name: 'Asim', blocked: true},
+      {name: 'Azhar', blocked: false},
+    ]
+  }),
   created(){
     console.log(process.env.NUXT_API_URL);
+  },
+  computed:{
+    getUser(){
+      return _.filter(this.users, (user)=>{return !user.blocked})
+    }
   }
+
 }
 </script>
